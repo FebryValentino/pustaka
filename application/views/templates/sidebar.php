621ccdecb8dd5ddf84818937da4e69e41c3945b2
FebryValentino <febryvalentino18@gmail.com>
2022-05-28 01:43:47 +0700

    .

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -22,7 +22,7 @@
                 <!-- Nav Item - Dashboard -->
             <li class="nav-item">
                 <a class="nav-link pb-0" href="<?= base_url('admin'); ?>">
-                    <i class="fa fa-fw fa book"></i>
+                    <i class="fa fa-fw fa-home"></i>
                     <span>Dashboard</span></a>
             </li>
             </li>
@@ -39,17 +39,17 @@
                 <!-- Nav Item - Dashboard -->
             <li class="nav-item">
                 <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
-                    <i class="fa fa-fw fa book"></i>
+                    <i class="fa fa-fw fa-book"></i>
                     <span>Kategori Buku</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link pb-0" href="<?= base_url('buku'); ?>">
-                    <i class="fa fa-fw fa book"></i>
+                    <i class="fa fa-fw fa-book"></i>
                     <span>Data Buku</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link pb-0" href="<?= base_url('user/anggota'); ?>">
-                    <i class="fa fa-fw fa book"></i>
+                    <i class="fa fa-fw fa-book"></i>
                     <span>Data Anggota</span></a>
             </li>
             </li>
@@ -74,6 +74,33 @@
                     <span>Data Booking</span></a>
             </li>
             </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider mt-3">
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Laporan
+            </div>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_buku'); ?>">
+                    <i class="fa fa-fw fa-address-book"></i>
+                    <span>Laporan Data Buku</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_anggota'); ?>">
+                    <i class="fa fa-fw fa-address-book"></i>
+                    <span>Laporan Data Anggota</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_pinjam'); ?>">
+                    <i class="fa fa-fw fa-address-book"></i>
+                    <span>Laporan Peminjaman</span></a>
+            </li>
+            </li>
+
 
             <!-- Divider -->
             <hr class="sidebar-divider mt-3">
